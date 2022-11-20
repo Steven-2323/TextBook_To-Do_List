1207fe4235a2bd262ce1f77244333e7cac98551d
Steven-2323 <71292794+Steven-2323@users.noreply.github.com>
2022-11-20 12:07:23 -0500

    Update MainActivity.java
    Fixed issues with imports for fab button, left space for fab functionality to be created, removed text edit and add buttons so fab is only functionality to create task, left internal file and clear button code because I thought that that could still be useful, no navigation code inserted for nav fragment because I am not sure if it will be necessary yet

--- app/src/main/java/com/zybooks/to_dolist/MainActivity.java
+++ app/src/main/java/com/zybooks/to_dolist/MainActivity.java
@@ -1,17 +1,16 @@
 package com.zybooks.to_dolist;
+package com.zybooks.to_dolistfinalapp;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import java.io.IOException;
-import com.zybooks.to_dolist.databinding.ActivityMainBinding;
+
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private ToDoList mToDoList;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.clear_button).setOnClickListener(view -> clearButtonClick());
 
         mToDoList = new ToDoList(this);
-        
+
         //sets the on click listener for the fab button; replace the snackbar code with the fab code
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -39,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
         try {
             // Attempt to load a previously saved list
             mToDoList.readFromFile();
-            displayList();
         }
         catch (IOException ex) {
             ex.printStackTrace();
@@ -61,6 +59,5 @@ public class MainActivity extends AppCompatActivity {
 
     private void clearButtonClick() {
         mToDoList.clear();
-        displayList();
     }
 }
